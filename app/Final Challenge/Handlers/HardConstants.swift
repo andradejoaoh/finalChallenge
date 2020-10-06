79a6186cb82d88587386fcba13d7f4fea680d946
Luiz Henrique Monteiro de Carvalho <lhmcarvx5@gmail.com>
2020-10-06 15:06:01 -0300

    Tela com constraints e table view setada com contato e perfil

--- app/Final Challenge/Handlers/HardConstants.swift
+++ app/Final Challenge/Handlers/HardConstants.swift
@@ -29,6 +29,10 @@ struct HardConstants {
         static let paidAnnoucementCellSection = "paidAnnouncementCellSection"
     }
     
+    struct TableView {
+        static let contactPerfilCell = "contactPerfilCell"
+    }
+    
     struct PickerView {
         static let productType = ["Comida", "Artesanato", "Pintura"]
         static let annoucementTime = ["1 hora", "2 horas", "4 horas", "6 horas", "8 horas", "12 horas", "16 horas", "1 dia", "2 dias"]
--- app/Final Challenge/View Controllers/AnnoucementViewController.swift
+++ app/Final Challenge/View Controllers/AnnoucementViewController.swift
@@ -8,7 +8,9 @@
 
 import UIKit
 
-class AnnoucementViewController: UIViewController, UIActionSheetDelegate {
+class AnnoucementViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UIActionSheetDelegate {
+    
+    
     var annoucement: Annoucement?
     var user: User?
     
@@ -16,6 +18,10 @@ class AnnoucementViewController: UIViewController, UIActionSheetDelegate {
     @IBOutlet weak var annoucementName: UILabel!
     @IBOutlet weak var annoucementDescription: UILabel!
     @IBOutlet weak var annoucementPrice: UILabel!
+    @IBOutlet weak var contactPerfilTableView: UITableView!
+    
+    let contactPerfilLabelArray: [String] = ["Contato", "Perfil"]
+    let contactPerfilImagesArray: [UIImage] = [#imageLiteral(resourceName: "contactIconPhone"), #imageLiteral(resourceName: "contactIconPhone")]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,6 +40,12 @@ class AnnoucementViewController: UIViewController, UIActionSheetDelegate {
             annoucementDescription.text = annoucement.description
             annoucementImage.image = UIImage(data: annoucement.imageData ?? Data())
         }
+        self.contactPerfilTableView.delegate = self
+        self.contactPerfilTableView.dataSource = self
+        self.contactPerfilTableView.isScrollEnabled = false
+        
+        self.contactPerfilTableView.backgroundColor = #colorLiteral(red: 0.9803921569, green: 0.9803921569, blue: 0.9803921569, alpha: 1)
+        self.contactPerfilTableView.layer.cornerRadius = 10
     }
     
     @IBAction func optionsAction(_ sender: Any) {
@@ -102,4 +114,42 @@ class AnnoucementViewController: UIViewController, UIActionSheetDelegate {
             }
         })
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        guard let cell = contactPerfilTableView.dequeueReusableCell(withIdentifier: HardConstants.TableView.contactPerfilCell, for: indexPath) as? AnnoucementDetailTableViewCell else { return UITableViewCell()}
+        cell.labelCell.text = contactPerfilLabelArray[indexPath.row]
+        cell.imagesCell.image = contactPerfilImagesArray[indexPath.row]
+        //self.contactPerfilTableView.backgroundColor = #colorLiteral(red: 0.9803921569, green: 0.9803921569, blue: 0.9803921569, alpha: 1)
+
+        cell.backgroundColor = #colorLiteral(red: 0.9803921569, green: 0.9803921569, blue: 0.9803921569, alpha: 1)
+        return cell
+        
+        //guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: HardConstants.CollectionView.annoucementCellSection, for: indexPath) as? annoucementCellForSection else { return UICollectionViewCell()}
+        
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        if indexPath.row == 0 {
+            
+        }
+        else if indexPath.row == 1 {
+            
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 55.0
+    }
+    
+    @IBAction func dismissButtonAction(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    
 }
