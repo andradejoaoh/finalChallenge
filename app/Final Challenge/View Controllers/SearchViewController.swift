7fe2985901760c4eceef252fe609fe8c17faf23e
Luiz Henrique Monteiro de Carvalho <lhmcarvx5@gmail.com>
2020-10-27 15:42:16 -0300

    Parte de bairros e proximidade comecadas

--- app/Final Challenge/View Controllers/SearchViewController.swift
+++ app/Final Challenge/View Controllers/SearchViewController.swift
@@ -12,9 +12,15 @@ class SearchViewController: UIViewController {
 
     @IBOutlet weak var ExpandableView: UIView!
     
+    @IBOutlet weak var ExpandableProximityView: UIView!
+    
     @IBOutlet weak var heightOfExpandableViewConstraint: NSLayoutConstraint!
     
+    @IBOutlet weak var verTudoButtonOutlet: UIButton!
+    @IBOutlet weak var heightOfProximityExpandableViewConstraint: NSLayoutConstraint!
+    
     var expandableCategoryControl: Bool = false
+    var expandableProximityControl: Bool = false
     
     @IBOutlet weak var scrollView: UIScrollView!
     override func viewDidLoad() {
@@ -24,22 +30,45 @@ class SearchViewController: UIViewController {
     }
     @IBAction func expandCategoryButtonAction(_ sender: Any) {
         if expandableCategoryControl == false {
-            UIView .animate(withDuration: 0.3) {
+            UIView.animate(withDuration: 0.3) {
                 self.heightOfExpandableViewConstraint.constant = 250.0
                 self.view.setNeedsUpdateConstraints()
+                self.verTudoButtonOutlet.setTitle("Minimizar", for: .normal)
                 self.view.layoutIfNeeded()
             }
+            
             self.scrollView.isScrollEnabled = true
             expandableCategoryControl = true
         } else {
             UIView .animate(withDuration: 0.3) {
                 self.heightOfExpandableViewConstraint.constant = 0.0
                 self.view.setNeedsUpdateConstraints()
+                self.verTudoButtonOutlet.setTitle("Ver tudo", for: .normal)
                 self.view.layoutIfNeeded()
             }
+            
             scrollView.setContentOffset(.zero, animated: true)
             expandableCategoryControl = false
             self.scrollView.isScrollEnabled = false
+        }
+    }
+    
+    
+    @IBAction func proximityExpandableAction(_ sender: Any) {
+        if expandableProximityControl == false {
+            UIView.animate(withDuration: 0.3) {
+                self.heightOfProximityExpandableViewConstraint.constant = 100.0
+                self.view.setNeedsUpdateConstraints()
+                self.view.layoutIfNeeded()
+            }
+            expandableProximityControl = true
+        } else {
+            UIView.animate(withDuration: 0.3) {
+                self.heightOfProximityExpandableViewConstraint.constant = 0
+                self.view.setNeedsUpdateConstraints()
+                self.view.layoutIfNeeded()
+            }
+            expandableProximityControl = false
         }
     }
 }
