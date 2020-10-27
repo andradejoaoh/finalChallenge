3a0f18f036457ee564f38c138902cb37dbc5b61e
Luiz Henrique Monteiro de Carvalho <lhmcarvx5@gmail.com>
2020-10-27 17:57:31 -0300

    Slider ok e ajustes na tela de categoria

--- app/Final Challenge/View Controllers/SearchViewController.swift
+++ app/Final Challenge/View Controllers/SearchViewController.swift
@@ -14,19 +14,33 @@ class SearchViewController: UIViewController {
     
     @IBOutlet weak var ExpandableProximityView: UIView!
     
+    @IBOutlet weak var sliderProximity: UISlider!
     @IBOutlet weak var heightOfExpandableViewConstraint: NSLayoutConstraint!
     
     @IBOutlet weak var verTudoButtonOutlet: UIButton!
     @IBOutlet weak var heightOfProximityExpandableViewConstraint: NSLayoutConstraint!
     
+    @IBOutlet weak var sliderLabel: UILabel!
     var expandableCategoryControl: Bool = false
     var expandableProximityControl: Bool = false
     
     @IBOutlet weak var scrollView: UIScrollView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.scrollView.isScrollEnabled = false
+        setup()
         // Do any additional setup after loading the view.
+    }
+    
+    func setup(){
+        self.scrollView.isScrollEnabled = false
+        sliderProximity.value = 6
+        
+        self.sliderLabel.text = "\(sliderProximity.value - 1) km"
+        let trackRect = sliderProximity.trackRect(forBounds: sliderProximity.frame)
+        let thumbRect = sliderProximity.thumbRect(forBounds: sliderProximity.bounds, trackRect: trackRect, value: sliderProximity.value)
+        
+        self.sliderLabel.center = CGPoint(x: thumbRect.midX, y: self.sliderLabel.center.y)
+        
     }
     @IBAction func expandCategoryButtonAction(_ sender: Any) {
         if expandableCategoryControl == false {
@@ -71,4 +85,19 @@ class SearchViewController: UIViewController {
             expandableProximityControl = false
         }
     }
+    
+    @IBAction func sliderProximityAction(_ sender: UISlider) {
+        
+        sliderProximity.value = roundf(sliderProximity.value)
+        self.sliderLabel.text = "\(sliderProximity.value - 1) km"
+        if sliderProximity.value == 1 {
+            self.sliderLabel.text = "500 m"
+        }
+//        let trackRect = sliderProximity.trackRect(forBounds: sliderProximity.frame)
+//        let thumbRect = sliderProximity.thumbRect(forBounds: sliderProximity.bounds, trackRect: trackRect, value: sliderProximity.value)
+//
+//        self.sliderLabel.center = CGPoint(x: thumbRect.midX, y: self.sliderLabel.center.y)
+    }
+    
+    
 }
