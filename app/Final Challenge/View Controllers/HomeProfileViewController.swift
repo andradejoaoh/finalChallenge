daf9a5a73f28f3ee9b9b6ce98b06f0de50c76ee4
Luiz Henrique Monteiro de Carvalho <lhmcarvx5@gmail.com>
2020-11-05 14:13:05 -0300

    tela de login antes de grandes alteracoes no storyboard

--- app/Final Challenge/View Controllers/HomeProfileViewController.swift
+++ app/Final Challenge/View Controllers/HomeProfileViewController.swift
@@ -8,17 +8,27 @@
 
 import UIKit
 
-class HomeProfileViewController: UIViewController {
+class HomeProfileViewController: UIViewController, UITextFieldDelegate{
     @IBOutlet weak var loginButton: UIButton!
     @IBOutlet weak var signUpButton: UIButton!
+    
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        setupStyleElements()
+        
         if DatabaseHandler.isUserLoggedIn() {
             transitionToProfile()
         }
+        
+        self.emailTextField.delegate = self
+        self.passwordTextField.delegate = self
+        self.hideKeyboardWhenTappedAround()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -30,16 +40,72 @@ class HomeProfileViewController: UIViewController {
         }
     }
 
-    func setupStyleElements(){
-        StyleElements.styleFilledButton(loginButton)
-        StyleElements.styleFilledButton(signUpButton)
-    }
+    
     
     
     func transitionToProfile(){
         let profileViewController = storyboard?.instantiateViewController(identifier: HardConstants.Storyboard.profileViewController) as? ProfileViewController
         view.window?.rootViewController = profileViewController
         view.window?.makeKeyAndVisible()
+    }
+    
+    func transitionToProfile(){
+        if let profileViewController = self.storyboard?.instantiateViewController(withIdentifier: HardConstants.Storyboard.profileViewController), let navigationController = self.navigationController{
+            navigationController.setViewControllers([profileViewController], animated: true)
+        }
+    }
+    
+   
+    
+    @IBAction func loginAction(_ sender: Any) {
+        //Validate Text Fields
+        let error = self.validateFields()
+        
+        if error != nil {
+            //Show error
+        } else {
+            //Create clean data
+            let email = emailTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            let password = passwordTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            //SignIn with user credentials
+            DatabaseHandler.loginWithEmail(email: email, password: password) { (result) in
+                switch result {
+                case let .failure(error):
+                    //Show error while logging in
+                    print(error)
+                case .success:
+                    self.transitionToProfile()
+                }
+            }
+        }
+    }
+    
+    func validateFields() -> String? {
+        //Cheeck if fields are filled in
+        if emailTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||
+            passwordTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" {
+            return "Preencha todos os campos."
+        }
+        return nil
+    }
+    
+    
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == emailTextField {
+            textField.resignFirstResponder()
+            passwordTextField.becomeFirstResponder()
+        } else if textField == passwordTextField {
+            textField.resignFirstResponder()
+        }
+        return true
+    }
+    
+    
+    func setupStyleElements(){
+        StyleElements.styleTextField(emailTextField)
+        StyleElements.styleTextField(passwordTextField)
+        StyleElements.styleFilledButton(loginButton)
     }
 }
 
