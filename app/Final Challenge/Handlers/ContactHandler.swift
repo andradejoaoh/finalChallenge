1c28ed340c618c4ab11b8eb91ebdfbcdf4d209a9
João Henrique Andrade <andradejoaoh@gmail.com>
2020-11-25 16:45:13 -0300

    Adicionado botão redes sociais

--- app/Final Challenge/Handlers/ContactHandler.swift
+++ app/Final Challenge/Handlers/ContactHandler.swift
@@ -28,4 +28,35 @@ class ContactHandler {
         actionSheet.addAction(UIAlertAction(title: "Cancelar", style: .cancel, handler: nil))
         return actionSheet
     }
+    
+    static func createSocialController(to user: User) -> UIAlertController {
+        if user.userTelegram == "" && user.userInstagram == "" && user.userFacebook == "" {
+            let actionSheet = UIAlertController(title: "Redes Sociais Indisponíveis", message: "Usuário não informou suas redes sociais.", preferredStyle: .actionSheet)
+            actionSheet.addAction(UIAlertAction(title: "Cancelar", style: .cancel, handler: nil))
+            return actionSheet
+        }
+        let actionSheet = UIAlertController(title: "Redes Sociais", message: "Escolha a rede social para entrar em contato.", preferredStyle: .actionSheet)
+        if user.userTelegram != "" && user.userTelegram != nil {
+            actionSheet.addAction(UIAlertAction(title: "Telegram", style: .default, handler: { (UIAlertAction) in
+                if let url = URL(string: "\(String(describing: user.userTelegram))"), UIApplication.shared.canOpenURL(url){
+                    UIApplication.shared.open(url)
+                }
+            }))
+        }
+        if user.userInstagram != "" && user.userInstagram != nil {
+            actionSheet.addAction(UIAlertAction(title: "Instagram", style: .default, handler: { (UIAlertAction) in
+                if let url = URL(string: "instagram.com/\(String(describing: user.userInstagram))"), UIApplication.shared.canOpenURL(url){
+                    UIApplication.shared.open(url)
+                }
+            }))
+        }
+        if user.userFacebook != "" && user.userFacebook != nil {
+            actionSheet.addAction(UIAlertAction(title: "Facebook", style: .default, handler: { (UIAlertAction) in
+                if let url = URL(string: "fb.com/\(String(describing: user.userFacebook))"), UIApplication.shared.canOpenURL(url){
+                    UIApplication.shared.open(url)
+                }
+            }))
+        }
+        return actionSheet
+    }
 }
